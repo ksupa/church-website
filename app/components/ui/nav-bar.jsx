7cd8243ef4b549682e37bafec26269d0e919594c
ksupa <kevinbsupa@gmail.com>
2025-09-16 14:04:42 -0600

    created values section

--- app/components/ui/nav-bar.jsx
+++ app/components/ui/nav-bar.jsx
@@ -7,7 +7,7 @@ export const NavBar = () => {
         <nav className="bg-white shadow-md fixed w-full z-10 heading-text font-semibold">
             <div className="max-w-7xl mx-auto px-8 py-6 flex justify-between items-center text-md md:text-xl">
               {/* FBCFI Edmonton or FBCFI Logo */}
-                <Link href="#home" className="text-gray-700 hover:text-green-800">
+                <Link href="/" className="text-gray-700 hover:text-green-800">
                   FBCFI Edmonton
                 </Link>
 
@@ -16,7 +16,10 @@ export const NavBar = () => {
                         <Link href="#home" className="hover:text-green-800">Home</Link>
                     </li>
                     <li>
-                        <Link href="#about" className="hover:text-green-800">About</Link>
+                        <Link href="#values" className="hover:text-green-800">Values</Link>
+                    </li>
+                    <li>
+                        <Link href="#" className="hover:text-green-800">About</Link>
                     </li>
                 </ul>
                 <button className='cursor-pointer border-2 hover:border-green-800 border-black bg-white hover:bg-green-800 text-black hover:text-white px-4 py-1 rounded-full'>
--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import banner from "../assets/banner.webp";
+import missionImg from "../assets/mission-img.webp";
 
 export default function Home() {
   return (
@@ -27,24 +28,39 @@ export default function Home() {
         </div>
       </section>
 
-      <section id="about" className="py-15 bg-gray-100">
-        <div className="max-w-7xl mx-auto pb-10 grid grid-cols-2">
-          <div>
+      <section id="values" className="py-15 bg-gray-100">
+        <div className="max-w-7xl mx-auto pb-10 grid grid-cols-2 gap-3">
+          <div className="px-5">
             <h2 className="text-3xl md:text-4xl lg:text-6xl pb-10 text-green-800 heading-text">Our Mission</h2>
-            <p className="text-black text-lg md:text-xl">
-              FBCFI is mandated to organize preachings, teachings, crusades, Gospel concerts and various ministries to plant churches and train leaders and workers to serve and build the Kingdom of God.
-            </p>
+            <div>
+              <p className="text-black text-lg md:text-xl">
+                FBCFI is mandated to organize preachings, teachings, crusades, Gospel concerts and various ministries to plant churches and train leaders and workers to serve and build the Kingdom of God.
+              </p>
+              <Image
+                src={missionImg}
+                alt="Woman at church with her hand raised"
+                className="mt-10 rounded-lg shadow-lg"
+                width={1200}
+                height={800}
+              />
+            </div>
           </div>
-        </div>
-
-        <div className="max-w-7xl mx-auto pb-10 grid grid-cols-2">
-          <div>
+          <div className="px-5"> 
             <h2 className="text-3xl md:text-4xl lg:text-6xl pb-10 text-green-800 heading-text">Our Vision</h2>
             <p className="text-black text-lg md:text-xl">
               FBCFI envision to proclaim, promote and advocate the full Gospel of Jesus Christ throughout the Philippines and Overseas by whatever means, form or manner in accordance with teh Word of God and leading of the Holy Spirit.
             </p>
+            <Image 
+              src="https://placehold.net/600x400.png"
+              width={1200}
+              height={800}
+              alt="Placeholder image"
+              className="mt-10 rounded-lg shadow-lg"
+            />
           </div>
         </div>
+
+          
       </section>
     </>
   );
